97fe1798562b99b161d480696cf62f642f642c77
QiuyuLian <lianqiuyu@lianqiuyus-MacBook-Pro.local>
2019-10-10 01:22:09 +0800

    mini version

---
+++ BTree.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 15:41:14 2019
+
+@author: lqyair
+"""
+
+class BTree:
+
+    def __init__(self, key, left = None, right = None, indices = None, \
+                 all_clustering_dic = None, where_dominant = None):
+        self.key = key # a str
+        self.right = right # a BstNode
+        self.left = left # a BstNode
+        self.indices = indices # a []
+        self.all_clustering_dic = all_clustering_dic
+        #self.marker_summary = marker_summary # a pd.df
+        #self.para = para # a {} parameters for qualified components
+        self.where_dominant = where_dominant # str ("left"/"right"), indicator of edge color
+        #self.stop = stop # legacy
+            
+    
+    def display(self):
+        lines, _, _, _ = self._display_aux()
+        for line in lines:
+            print(line)
+
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+        #if self.right.key is 'leaf' and self.left.key is 'leaf':
+            line = '%s' % '_'.join(self.key)
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+        #if self.right.key is 'leaf':
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % '_'.join(self.key)
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+        #if self.left.key is 'leaf':
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % '_'.join(self.key)
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % '_'.join(self.key)
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+    
+    
+
---
+++ matryoshka_open.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 23:49:04 2019
+
+@author: lianqiuyu
+"""
+
+import pandas as pd
+from Matryoshka import Matryoshka
+from Visualize import visualize_tree
+from BTreeTraversal import BTreeTraversal
+import pickle
+import argparse
+import os
+
+#from sys import argv
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('data_path', help = "The input path of data in .csv files with row as sample, col as feature.")
+    parser.add_argument('-c','--cutoff', help = "The cutoff for merging components (default 0.1). It shoube between 0 and 1. The bigger value leads to split more aggressively, and ends in a more complicated tree.")
+    parser.add_argument("-o", "--output", help="The path for storing the tree structure and leaf labels. Requires a path argument.", type=str)
+
+    args = parser.parse_args()
+    data_path = args.data_path
+    
+    if args.output:
+        output_path = args.output
+    else:
+        output_path = "./Matryoshka_out"
+    
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    
+    if args.cutoff:
+        merge_cutoff = int(args.cutoff)
+    else:
+        merge_cutoff = 0.1
+    
+    data = pd.read_csv(data_path,header = 0, index_col=0)
+    tree = Matryoshka(data,merge_cutoff)
+    visualize_tree(tree,data,output_path,'tree')
+    
+    f = open(output_path+'/tree.pickle','wb')
+    pickle.dump(tree,f)
+    f.close()
+    
+    traversal = BTreeTraversal(tree)
+    leaves_labels = traversal.get_leaf_label()
+    leaves_labels.to_csv(output_path + '/leaf_labels.csv')
+
+
+if __name__ == "__main__":
+    main()
+    
+    
